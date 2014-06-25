7a87ac34cace57904c49466f80823e9e2a52ec5a
Luis Perez <kandluis@yahoo.com>
2014-06-25 01:19:00 -0400

    Fully tested and working as expected. Still need ddg.source

--- Utilities/scriptTimer.r
+++ Utilities/scriptTimer.r
@@ -27,7 +27,7 @@ src.two <- list("/daily solar radiation", "daily-solar-radiation")
 src.three <- list("/seminar one", "R_REU_S1")
 src.four <- list("/seminar two", "R_REU_S2")
 src.six <- list("/seminar four", "RWorkshop-Session4")
-scripts <- list(src.zero, src.one, src.two,src.three, src.four)
+scripts <- list(src.zero, src.one, src.two,src.three, src.four, src.six)
 
 # results
 results <- lapply(scripts,function(x){
