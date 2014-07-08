d2f1cc0e2979b0211a3e4b30ed617abc336b1821
Luis Perez <kandluis@yahoo.com>
2014-07-08 13:18:31 -0400

    Including expected output files for additional tests

---
+++ examples/simpleTest/simple.r
@@ -0,0 +1,28 @@
+###
+# Very simple test script to assure us that ddg is still handling simple data as expected
+###
+
+
+library(RDataTracker)
+
+## Directories
+testDir <- "[DIR_DEFAULT]/"
+setwd(testDir)
+
+ddg.r.script.path = paste(testDir,"simple.r",sep="")
+ddg.path = paste(testDir,"ddg",sep="")
+
+# Initialize the provenance graph
+ddg.init(ddg.r.script.path,
+         ddg.path,
+    enable.console=FALSE)
+
+
+a <- 5
+ddg.data("a")
+b <- 5
+ddg.data("b")
+c <- a + b
+ddg.procedure("Add", ins=list("a","b"), outs.data=list("c"))
+
+ddg.save()
