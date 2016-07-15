d0dc2ffed8a56e9e3443680dc97f822f08da0ab6
tfjmp <thomas.francis.pasquier@gmail.com>
2016-07-15 09:28:13 -0400

    handle windows as well

--- R/RDataTracker.R
+++ R/RDataTracker.R
@@ -5639,7 +5639,11 @@ ddg.run <- function(r.script.path = NULL, ddgdir = NULL, overwrite = TRUE, f = N
 	index<- min(which(check.library.paths == TRUE))
 	ddgexplorer_path<- paste(.libPaths()[index],jar.path,sep = "")
 	ddgtxt.path<- paste(ddg.folder,"/ddg.txt",sep = "")
-	system(paste("java -jar ",ddgexplorer_path, ddgtxt.path,'&',sep = " "))
+	if(Sys.info()['sysname']=="Windows"){
+		system(paste("java -jar ",ddgexplorer_path, ddgtxt.path,'&',sep = " "))
+	}else{
+		system(paste("START java -jar ",ddgexplorer_path, ddgtxt.path,sep = " "))
+	}
 }
 
 # This function takes a Rmd file and extracts the R code and text through
