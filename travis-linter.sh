952cdf3eb469304ea64010ad2b9f6ab7ac0b9874
tfjmp <thomas.francis.pasquier@gmail.com>
2017-03-20 18:16:30 -0400

    update lintr invocation, lets see how it works

--- travis-linter.sh
+++ travis-linter.sh
@@ -3,14 +3,11 @@ set -e
 
 exitstatus=0
 
-for file in R/*.R
-do
-    Rscript -e "lintr::lint(\"$file\")"
-    outputbytes=`Rscript -e "lintr::lint(\"$file\")" | grep ^ | wc -c`
-    if [ $outputbytes -gt 0 ]
-    then
-        exitstatus=1
-    fi
-done
+Rscript -e "lintr::lint_package()"
+outputbytes=`Rscript -e "lintr::lint_package()" | grep ^ | wc -c`
+if [ $outputbytes -gt 0 ]
+then
+	exitstatus=1
+fi
 
 exit $exitstatus
