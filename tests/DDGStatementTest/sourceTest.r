91250f4c2100e685c4649dde7bae163df0f78335
fong22e <fong22e@mtholyoke.edu>
2017-06-06 16:09:42 -0400

    updated basicTest

--- tests/DDGStatementTest/sourceTest.r
+++ tests/DDGStatementTest/sourceTest.r
@@ -14,7 +14,7 @@ library(methods)
 library(RDataTracker)
 
 ## Directories
-testDir <- "C:/Users/fong22e/My Documents/HarvardForest/RDataTracker_typecoercion/tests/DDGStatementTest/"
+testDir <- "C:/Users/fong22e/Documents/HarvardForest/RDataTracker_debugRegressionTests/tests/DDGStatementTest/"
 setwd(testDir)
 
 # get initial time
@@ -22,8 +22,8 @@ startTime <- Sys.time()
 invisible(force(startTime))
 
 # Run the script
-#ddg.run("DDGStatementTest.R", "C:/Users/fong22e/My Documents/HarvardForest/RDataTracker_typecoercion/tests/DDGStatementTest/ddg", ignore.ddg.calls = F)
-ddg.run("DDGStatementTest.R", "C:/Users/fong22e/My Documents/HarvardForest/RDataTracker_typecoercion/tests/DDGStatementTest/ddg")
+#ddg.run("DDGStatementTest.R", "C:/Users/fong22e/Documents/HarvardForest/RDataTracker_debugRegressionTests/tests/DDGStatementTest/ddg", ignore.ddg.calls = F)
+ddg.run("DDGStatementTest.R", "C:/Users/fong22e/Documents/HarvardForest/RDataTracker_debugRegressionTests/tests/DDGStatementTest/ddg")
 
 # Calculate total time of execution
 endTime <- Sys.time()
