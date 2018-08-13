16257f216f47c5bc53022a77de81fa88c2b7d538
Barbara Lerner <blerner@mtholyoke.edu>
2018-08-13 17:06:16 -0400

    Added missing quote

--- travis-install.sh
+++ travis-install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-Rscript -e 'install.packages(c("akima", "chron", "dplR", "dplyr", "Ecdat", "Ecfun", "extrafont", "gdata", "ggplot2", "gplots", "grid", "gridExtra", "gWidgets", "jsonlite", "jsonvalidate", "mosaic", "plyr", "RCurl", "reshape2", "roxygen2", "shiny", "stringi", "stringr", "sysfonts", "xkcd", "zoo"))
+Rscript -e 'install.packages(c("akima", "chron", "dplR", "dplyr", "Ecdat", "Ecfun", "extrafont", "gdata", "ggplot2", "gplots", "grid", "gridExtra", "gWidgets", "jsonlite", "jsonvalidate", "mosaic", "plyr", "RCurl", "reshape2", "roxygen2", "shiny", "stringi", "stringr", "sysfonts", "xkcd", "zoo"))'
