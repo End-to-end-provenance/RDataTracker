9ef9b7b156ad0cca0cb6eb8bc1c613507440956a
Boose <boose@fas.harvard.edu>
2018-08-16 16:51:19 -0400

    Create separate Readme files

--- R/DDGStatement_prov.R
+++ R/DDGStatement_prov.R
@@ -1,3 +1,23 @@
+# Copyright (C) President and Fellows of Harvard College and 
+# Trustees of Mount Holyoke College, 2014, 2015, 2016, 2017.
+
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program.  If not, see
+#   <http://www.gnu.org/licenses/>.
+
+########################## DDGStatement.R ############################
+
+
 #' .ddg.construct.DDGStatement creates a DDGStatement.
 #' @param expr - the parsed expression
 #' @param pos - the DDGStatementPos object for this statement
@@ -16,4 +36,3 @@
   
   return (methods::new (Class = "DDGStatement", parsed = expr, pos, script.num))
 }
-
