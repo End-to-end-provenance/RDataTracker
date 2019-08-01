cf13dbf1c06ca7046eae897f9abfa71b34618e29
Barbara Lerner <blerner@mtholyoke.edu>
2019-08-01 13:47:42 -0400

    Added test for existence of json file before doing compliance test

--- scriptTests/ProvJson/complianceTest.r
+++ scriptTests/ProvJson/complianceTest.r
@@ -4,14 +4,5 @@ args <- commandArgs(TRUE)
 
 schema <- args[1]
 json <- args[2]
-print(schema)
-print(json)
-print (dir(dirname(json)))
 
-options (error=function() traceback(2))
-tryCatch (json_validate(json, schema, verbose=TRUE),
-    error = function (e) {
-      print (dir(dirname(json)))
-      lines <- readLines(json)
-      writeLines(lines)
-    })
+json_validate(json, schema, verbose=TRUE)
